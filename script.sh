302183de35bf286ed594b357131de6bade743fcc
Franzzz0 <fr.krauze@gmail.com>
2023-07-23 21:29:21 +0200

    stage 2

--- script.sh
+++ script.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env bash
-echo "Enter a message:"
-read -r input
-re='^[A-Z ]+$'
-if [[ "$input" =~ $re ]]; then
-    echo "This is a valid message!"
+encrypt() {
+    value=$(($(get_value "$1") + "$2"))
+    if [[ $value -gt 90 ]]; then
+        value=$((value - 26))
+    fi
+    letter=$(get_letter $value)
+    echo "Encrypted letter: $letter"    
+}
+get_value() {
+    printf "%d\n" "'$1"
+}
+get_letter() {
+    printf "%b\n" "$(printf "\\%03o" "$1")"
+}
+echo "Enter an uppercase letter:"
+read -r letter
+echo "Enter a key:"
+read -r key
+re_letter='^[A-Z]$'
+re_key='^[0-9]$'
+if [[ "$letter" =~ $re_letter && "$key" =~ $re_key ]]; then
+    encrypt "$letter" "$key"
 else
-    echo "This is not a valid message!"
+    echo "Invalid key or letter!"
 fi
